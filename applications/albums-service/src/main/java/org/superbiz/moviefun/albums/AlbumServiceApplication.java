011f99f6e4c3235dfebcf2d8fc1b4695dc8c349f
pal-6 <sarann93@gmail.com>
2019-04-25 13:15:57 +0530

    Microservices

--- applications/albums-service/src/main/java/org/superbiz/moviefun/albums/AlbumServiceApplication.java
+++ applications/albums-service/src/main/java/org/superbiz/moviefun/albums/AlbumServiceApplication.java
@@ -1,4 +1,4 @@
-package org.superbiz.moviefun;
+package org.superbiz.moviefun.albums;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
@@ -6,23 +6,18 @@ import com.amazonaws.services.s3.AmazonS3Client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.superbiz.cloudfoundry.ServiceCredentials;
 import org.superbiz.moviefun.blobstore.BlobStore;
 import org.superbiz.moviefun.blobstore.S3Store;
-import org.superbiz.moviefun.moviesapi.MovieServlet;
 
 @SpringBootApplication
-public class Application {
+public class AlbumServiceApplication {
 
-    public static void main(String... args) {
-        SpringApplication.run(Application.class, args);
-    }
+    public static void main(String[] args) {
 
-    @Bean
-    public ServletRegistrationBean actionServletRegistration(MovieServlet movieServlet) {
-        return new ServletRegistrationBean(movieServlet, "/moviefun/*");
+        SpringApplication.run(AlbumServiceApplication.class);
+
     }
 
     @Bean
@@ -48,4 +43,6 @@ public class Application {
 
         return new S3Store(s3Client, photoStorageBucket);
     }
+
+
 }
